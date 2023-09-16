a9af969c2042946642caa22bf2506630b98e7218
Ronald Kelechi <ronaldkelechi11@gmail.com>
2023-09-16 19:55:17 +0100

    First stage changes after namechange

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wexchange-backend
- 
+Company name change from WeXchange to Selln
+
+DBMS: Mongo DB
+Framework: Express Js
+
+## Please remeber to add a PORT Variable in the Hosting Env variable. by default 3000
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
-const client = require('./providers/postgress');
 const cors = require('cors');
 
 const app = express()
-const port = 3000
+const mongoDBurl = ""
+const port = 3000 || process.env.PORT
 
 //Middleware
 app.use(express.urlencoded())
 app.use(express.json())
+app.use(cors({
+    origin: "*"
+}))
 
 //Providing Routes
 const SignUp = require('./routes/signup');
@@ -19,10 +22,5 @@ app.use('/login', Login)
 
 
 app.listen(port, () => {
-    console.log(`Backend Service listening on port ${port}`),
-        client.connect().then((result) => {
-            console.log('Connected to Postgress DB');
-        }).catch((err) => {
-            console.log("Error connecting to Postgress DB");
-        });
+    console.log(`Backend Service listening on port ${port}`)
 });
